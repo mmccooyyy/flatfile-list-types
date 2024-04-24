04f18fe1aae3c249c38b0e0515e12999e238baba
Matt McCoy <matthew.mccoy@flatfile.io>
2024-04-24 14:14:07 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,82 @@
+import api from "@flatfile/api";
+import * as Flatfile from "@flatfile/api/api";
+import { Client, FlatfileEvent, FlatfileListener } from "@flatfile/listener";
+
+export default function flatfileEventListener(listener: Client) {
+  listener.filter({ job: "space:configure" }, (configure: FlatfileListener) => {
+    configure.on(
+      "job:ready",
+      async ({ context: { spaceId, environmentId, jobId } }: FlatfileEvent) => {
+        try {
+          await api.jobs.ack(jobId, {
+            info: "Job started.",
+            progress: 10,
+          })
+
+          const testSheet = {
+              name: 'Test Sheet',
+              slug: 'testSheet',
+              fields: [
+                {
+                  key: 'string',
+                  type: 'string',
+                  label: 'String',
+                },
+                {
+                  key: 'number',
+                  type: 'number',
+                  label: 'Number',
+                },
+                {
+                  key: 'enum',
+                  type: 'enum',
+                  label: 'Enum',
+                  config: {
+                    options: [
+                      {
+                        value: 'red',
+                        label: 'Red',
+                      },
+                      {
+                        value: 'blue',
+                        label: 'Blue',
+                      },
+                      {
+                        value: 'green',
+                        label: 'Green',
+                      },
+                    ]
+                  }
+                },
+                {
+                  key: 'stringList',
+                  type: 'string-list',
+                  label: 'String List',
+                },
+              ],
+            } as Flatfile.SheetConfig
+
+          await api.workbooks.create({
+            name: 'Testing List Types',
+            spaceId,
+            environmentId,
+            sheets: [testSheet],
+          })
+
+          await api.jobs.complete(jobId, {
+            outcome: {
+              message: "Job completed.",
+            },
+          })
+        } catch (error: any) {
+          console.error(error)
+          await api.jobs.fail(jobId, {
+            outcome: {
+              message: "Job error.",
+            },
+          })
+        }
+      }
+    );
+  });
+}
